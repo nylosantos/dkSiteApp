6a74059a331804ec4098bdc99c5d8bcbff36cf04
Nylo Santos <nylopersan@gmail.com>
2022-09-30 05:30:54 +0200

    :pencil: fixing minor assets

--- src/components/menuModal/MenuContent.tsx
+++ src/components/menuModal/MenuContent.tsx
@@ -1,33 +1,36 @@
 import { useState } from "react";
+import { Fade } from "react-awesome-reveal";
 import { Link } from "react-router-dom";
 import { ContactModal } from "../contactModal/ContactModal";
 
 type Props = {
-  style: string
-}
+  style: string;
+};
 
-export function MenuContent({style}: Props) {
+export function MenuContent({ style }: Props) {
   const [isOpen, setIsOpen] = useState(false);
   const closeInstructionModal = () => setIsOpen(false);
   return (
     <div className="w-full flex flex-col justify-center items-center">
       <ContactModal isOpen={isOpen} onClose={closeInstructionModal} />
       <ol className={style}>
-        <Link to={"/"} onClick={closeInstructionModal}>
-          <li>Home</li>
-        </Link>
-        <Link to={"/About"} onClick={closeInstructionModal}>
-          <li>About</li>
-        </Link>
-        <Link to={"/Team"} onClick={closeInstructionModal}>
-          <li>Our Teamys</li>
-        </Link>
-        <Link to={"/Portfolio"} onClick={closeInstructionModal}>
-          <li>Portfolio</li>
-        </Link>
-        <li className="cursor-pointer" onClick={() => setIsOpen(true)}>
-          Contact
-        </li>
+        <Fade cascade={true}>
+          <Link to={"/"} onClick={closeInstructionModal}>
+            <li>Home</li>
+          </Link>
+          <Link to={"/About"} onClick={closeInstructionModal}>
+            <li>About</li>
+          </Link>
+          <Link to={"/Team"} onClick={closeInstructionModal}>
+            <li>Our Teamys</li>
+          </Link>
+          <Link to={"/Portfolio"} onClick={closeInstructionModal}>
+            <li>Portfolio</li>
+          </Link>
+          <li className="cursor-pointer" onClick={() => setIsOpen(true)}>
+            Contact
+          </li>
+        </Fade>
       </ol>
     </div>
   );
